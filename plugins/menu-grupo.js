797a2821526520107d4e2afef58e38f25f4029e1
Tiago <104955231+TiagoKl@users.noreply.github.com>
2022-07-14 13:35:53 -0500

    ☠️ 𝐃𝐀𝐓𝐀𝐎𝐍𝐄-𝐁𝐎𝐓

---
+++ plugins/menu-grupo.js
@@ -0,0 +1,44 @@
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+let handler = async (m, { conn, usedPrefix }) => {
+let pp = './Menu2.jpg'
+try {
+} catch (e) {
+} finally {
+//let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+let username = await conn.getName(m.sender)
+let str = `
+☠️ 𝐃𝐀𝐓𝐀𝐎𝐍𝐄-𝐁𝐎𝐓
+
+*𝗛𝗢𝗟𝗔 _${username}_ *
+
+𝗕𝗜𝗘𝗡𝗩𝗘𝗡𝗜𝗗𝗢 𝗔𝗟 𝗠𝗘𝗡𝗨 𝗚𝗥𝗨𝗣𝗢𝗦
+
+° ඬ⃟💎 _${usedPrefix}add *<numero>*_
+° ඬ⃟💎 _${usedPrefix}kick *<@tag>*_
+° ඬ⃟💎 _${usedPrefix}grupo *<abrir / cerrar>*_
+° ඬ⃟💎 _${usedPrefix}promote *<@tag>*_
+° ඬ⃟💎 _${usedPrefix}demote *<@tag>*_
+° ඬ⃟💎 _admins *<texto>*_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
+° ඬ⃟💎 _${usedPrefix}demote *<@tag>*_
+° ඬ⃟💎 _${usedPrefix}infogroup_
+° ඬ⃟💎 _${usedPrefix}link_
+° ඬ⃟💎 _${usedPrefix}setname *<texto>*_
+° ඬ⃟💎 _${usedPrefix}setdesc *<texto>*_
+° ඬ⃟💎 _${usedPrefix}invocar *<texto>*_
+° ඬ⃟💎 _${usedPrefix}setwelcome *<texto>*_
+° ඬ⃟💎 _${usedPrefix}setbye *<texto>*_
+° ඬ⃟💎 _${usedPrefix}hidetag *<texto>*_
+`.trim()
+conn.sendHydrated(m.chat, str, wm, pp, null, null, null, null, [
+['🗒️𝗠𝗘𝗡𝗨 𝗟𝗜𝗦𝗧𝗔🗒️', '.menu'],
+['🥵𝗖𝗥𝗘𝗔𝗗𝗢𝗥🥵', '.menu'],
+['📮𝗗𝗢𝗡𝗔𝗥📮', '.donar']
+], m)
+}}
+handler.help = ['menugp']
+handler.tags = ['main']
+handler.command = /^(menugrupos|menugrupo|memugrupos|menugrupos|menugp|memugp|comandosgrupo)$/i
+handler.fail = null
+handler.register = false 
+export default  handler
